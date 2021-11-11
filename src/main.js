cb6fda39cfa5ce889327d1ca862b68d1a8c2709d
xzq <xuzhiqiang.johnson@bigo.sg>
2021-11-11 17:37:36 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,58 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
-createApp(App).mount('#app')
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      //必须得加这一行，不然在index.html中导入ipcRenderer会出错
+      contextIsolation: false
+    },
+  });
+  mainWindow.loadFile('public/index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+  //打开开发者
+  mainWindow.webContents.openDevTools();
+
+  //检查更新，有的话会自动下载
+  mainWindow.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  });
+}
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+//
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
